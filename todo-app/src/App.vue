7cd92c04ab0e7dfbd0a8f9b440abce14970892fe
Lami <Halam.Lee.Lami@gmail.com>
2023-08-29 10:16:59 +0900

    finish

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,47 +1,117 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
+
+  input_content.value = '';
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem('todos', JSON.stringify(newVal));
+  },
+  { deep: true }
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+  todos.value = JSON.parse(localStorage.getItem('todos')) || [];
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Whats'up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. read a book"
+          v-model="input_content" />
 
-  <main>
-    <TheWelcome />
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
