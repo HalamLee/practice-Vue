88c555ac8c47adb99405e5076024046e0ef71244
Lami <Halam.Lee.Lami@gmail.com>
2023-09-04 08:24:29 +0900

    feat: parent, child component

---
+++ learn-vue3/src/App_Parent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="parent">
+    <h1>I'm Parent Component</h1>
+    <ChildComponent :num-arr="[1, 2, 3]" @child-num-arr="handleChildNumArr" />
+    <h2>Child - emit -> Parent: {{ numArr }}</h2>
+  </div>
+</template>
+
+<script setup>
+import ChildComponent from './ChildComponent.vue';
+import { ref } from 'vue';
+
+const numArr = ref([]);
+const handleChildNumArr = (changedNumArr) => {
+  numArr.value = changedNumArr;
+};
+</script>
+
+<style scoped>
+.parent {
+  background-color: rgb(226, 187, 44);
+  padding: 20px;
+}
+</style>
---
+++ learn-vue3/src/ChildComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child">
+    <h1>I'm Child Component</h1>
+    <h2>Parent - props -> Child: {{ numArr }}</h2>
+    <button @click="clickHandler">클릭</button>
+  </div>
+</template>
+
+<script setup>
+const { numArr } = defineProps({ numArr: Array });
+
+const emit = defineEmits(['childNumArr']);
+
+const clickHandler = () => {
+  emit(
+    'childNumArr',
+    numArr.map((num) => num * 2),
+  );
+};
+</script>
+
+<style scoped>
+.child {
+  background-color: aquamarine;
+  padding: 5px;
+}
+</style>
