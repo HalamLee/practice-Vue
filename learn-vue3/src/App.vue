a9e10af58cb0ed44a4c6f25d2505ac37800ee9df
Lami <Halam.Lee.Lami@gmail.com>
2023-09-04 15:03:26 +0900

    feat: component, emit

---
+++ learn-vue3/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <TheNav />
+    <TheView />
+  </div>
+</template>
+
+<script setup>
+import TheNav from './components/TheNav.vue';
+import TheView from './components/TheView.vue';
+</script>
+
+<style lang="scss" scoped></style>
