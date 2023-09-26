208dc68cebe293afdf347db6c4cb6770618d98da
Lami <Halam.Lee.Lami@gmail.com>
2023-09-26 17:15:43 +0900

    feat: pinia store 생성

---
+++ todo-app-with-pinia/src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
+</template>
+
+<script setup>
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
+</script>
+
+<style lang="scss" scoped></style>
