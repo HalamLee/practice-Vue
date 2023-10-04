95f3f23a3d1ed22a77a4e25b41f78b616e552d16
Lami <Halam.Lee.Lami@gmail.com>
2023-10-04 11:00:45 +0900

    fix: actions 밖에 있던 함수들 actions 객체 안으로 이동

--- todo-app-with-pinia/src/stores/todoList.js
+++ todo-app-with-pinia/src/stores/todoList.js
@@ -9,16 +9,16 @@ export const useTodoListStore = defineStore('todoList', {
     addTodo(item) {
       this.todoList.push({ item, id: this.id++, completed: false });
     },
-  },
-  deleteTodo(itemId) {
-    this.todoList = this.todoList.filter((object) => {
-      return object.id !== itemId;
-    });
-  },
-  toggleCompleted(idToFind) {
-    const todo = this.todoList.find((obj) => obj.id === idToFind);
-    if (todo) {
-      todo.completed = !todo.completed;
-    }
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((object) => {
+        return object.id !== itemId;
+      });
+    },
+    toggleCompleted(idToFind) {
+      const todo = this.todoList.find((obj) => obj.id === idToFind);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
   },
 });
