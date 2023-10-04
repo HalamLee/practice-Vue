39904927ea1ef0020a7e3aa383e0aedec5e5bd38
Lami <Halam.Lee.Lami@gmail.com>
2023-10-04 09:52:41 +0900

    feat: 할 일 추가 action 생성

--- todo-app-with-pinia/src/stores/todoList.js
+++ todo-app-with-pinia/src/stores/todoList.js
@@ -3,5 +3,11 @@ import { defineStore } from 'pinia';
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
     todoList: [],
+    id: 0,
   }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false });
+    },
+  },
 });
