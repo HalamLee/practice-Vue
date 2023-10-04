b92af7e0de98584c33f31c913a0cb29f94e5dcd4
Lami <Halam.Lee.Lami@gmail.com>
2023-10-04 11:10:43 +0900

    design: 전체적인 스타일링 마무리

--- todo-app-with-pinia/src/components/TodoApp.vue
+++ todo-app-with-pinia/src/components/TodoApp.vue
@@ -11,4 +11,11 @@ import TodoForm from './TodoForm.vue';
 import TodoList from './TodoList.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
+}
+</style>
