9b519f0c58f9b9b74fd543dd1f4a1cf76deff42a
Lami <Halam.Lee.Lami@gmail.com>
2023-10-04 10:07:16 +0900

    feat: 투두리스트 폼 생성 및 스타일링

--- todo-app-with-pinia/src/components/TodoForm.vue
+++ todo-app-with-pinia/src/components/TodoForm.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input type="text" v-model="todo" />
+      <button>Add</button>
+    </form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { useTodoListStore } from '@/stores/todoList';
+
+const todo = ref('');
+
+const store = useTodoListStore();
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return;
+  }
+  store.addTodo(item);
+  todo.value = '';
+}
+</script>
 
 <style lang="scss" scoped></style>
